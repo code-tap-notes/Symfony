4a126022949f0def4752e1932668a26757e972e3
code-tap-notes <nguyentuongvien@yahoo.com>
2025-02-05 16:49:20 +0100

    requirements validate data tren route, thu nghiem bang route http://localhost:8000/categorie/phongdoi- delux-1

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 final class CategorieController extends AbstractController
 {
-    #[Route('/categorie/{slug}-{id}', name: 'categorie.show')]
+    #[Route('/categorie/{slug}-{id}', name: 'categorie.show', requirements: ['id' => '\d+','slug' => '[a-z0-9-]+'])]
     public function index(request $request): Response
     {
         dd($request->attributes->get('slug'),$request->attributes->get('id'));
