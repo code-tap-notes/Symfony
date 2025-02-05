0538d7d9138ee481d9cfac92372bead5a70a96bb
code-tap-notes <nguyentuongvien@yahoo.com>
2025-02-05 16:51:15 +0100

    requirements validate data tren route, lay so nguyen o id, thu nghiem bang route http://localhost:8000/categorie/phongdoi- delux-1

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -11,6 +11,6 @@ final class CategorieController extends AbstractController
     #[Route('/categorie/{slug}-{id}', name: 'categorie.show', requirements: ['id' => '\d+','slug' => '[a-z0-9-]+'])]
     public function index(request $request): Response
     {
-        dd($request->attributes->get('slug'),$request->attributes->get('id'));
+        dd($request->attributes->get('slug'),$request->attributes->getInt('id'));
     }
 }
