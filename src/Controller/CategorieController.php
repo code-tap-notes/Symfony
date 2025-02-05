72c97638c87fe45d545d4f29f981810bdf3ac354
code-tap-notes <nguyentuongvien@yahoo.com>
2025-02-05 17:34:02 +0100

    dua du lieu ra templates

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -12,14 +12,16 @@ final class CategorieController extends AbstractController
     #[Route('/categorie', name: 'categorie.index')]
     public function index(request $request): Response
     {
-       return new Response('Categorie');
+       return $this->render('categorie/index.html.twig');
     }
     #[Route('/categorie/{slug}-{id}', name: 'categorie.show', requirements: ['id' => '\d+','slug' => '[a-z0-9-]+'])]
     public function show(request $request, string $slug, int $id): Response
     {
-        return $this->json([
-            'slug' => $slug
+        return $this->render('categorie/show.html.twig',
+            [
+            'slug' => $slug,
+            'id' => $id
         ]);
-        return new Response('Categorie'.$slug);
+        
     }
 }
