0b86b4ed3d7041691332aa00874d7a6c3a103836
code-tap-notes <nguyentuongvien@yahoo.com>
2025-02-05 13:27:34 +0100

    Khi muon dua tham so bang cach su dung thu vien Request, phai khai bao use Symfony\Component\HttpFoundation\Request; va ghi tham so vao route vi du localhost:8000/?name=Vien

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
@@ -17,8 +17,8 @@ final class HomeController extends AbstractController
         ]);
     }
     #[Route('/', name: 'chao')]
-    public function chao(): Response
+    public function chao(Request $request): Response
     {
-        return new Response('Bonjour'.$_GET['name']);
+        return new Response('Bonjour'.$request->query->get('name'));
     }
 }
