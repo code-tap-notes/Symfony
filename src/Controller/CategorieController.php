7db7697fd6898b9ac4c615af6ab59e1cf34a0714
code-tap-notes <nguyentuongvien@yahoo.com>
2025-02-05 17:04:00 +0100

    sortie Json

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -4,13 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 final class CategorieController extends AbstractController
 {
-    #[Route('/categorie/{slug}-{id}', name: 'categorie.show', requirements: ['id' => '\d+','slug' => '[a-z0-9-]+'])]
-    public function index(request $request, string $slug, int $id): Response
+    #[Route('/categorie', name: 'categorie.index')]
+    public function index(request $request): Response
     {
-        dd($slug,$id);
+       return new Response('Categorie');
+    }
+    #[Route('/categorie/{slug}-{id}', name: 'categorie.show', requirements: ['id' => '\d+','slug' => '[a-z0-9-]+'])]
+    public function show(request $request, string $slug, int $id): Response
+    {
+        return NEW jsonResponse([
+            'slug' => $slug
+        ]);
+        return new Response('Categorie'.$slug);
     }
 }
