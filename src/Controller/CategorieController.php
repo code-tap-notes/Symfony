17feff087ab1469bf14b618ff8ac07e4551027cc
code-tap-notes <nguyentuongvien@yahoo.com>
2025-02-05 16:26:05 +0100

    Tao controller Categorie va ghi tham so vao duong dan, thu bien request bang route http://localhost:8000/categorie/phongdoi-1

---
+++ src/Controller/CategorieController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+final class CategorieController extends AbstractController
+{
+    #[Route('/categorie/{slug}-{id}', name: 'categorie.show')]
+    public function index(request $request): Response
+    {
+        dd($request);
+    }
+}
