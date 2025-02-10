1b1ebcac9fde73f4924466e2a2550d5c356d3ff3
code-tap-notes <nguyentuongvien@yahoo.com>
2025-02-10 17:19:42 +0100

    dung EntityMAnagerInterface de tao 1 new record

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -1,21 +1,39 @@
 <?php
 
 namespace App\Controller;
-
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Repository\CategorieRepository;
+use App\Entity\Categorie;
+
 final class CategorieController extends AbstractController
 {
     #[Route('/categories', name: 'categorie.index')]
-    public function index(Request $request, CategorieRepository $repository): Response
+    public function index(Request $request, CategorieRepository $repository, EntityManagerInterface $em): Response
     {
-       $categories = $repository->findAll();
+        $categorie = new Categorie();
+        $categorie->setCode('P9LIII')->setDescription('Chambre falille pour 9 persons');
+        $em->persist($categorie);
+        $em->flush(); 
+        $categories = $repository->findAll();
+       
        return $this->render('categorie/index.html.twig',[
         'categories' => $categories
+    ]);
+    }
+    #[Route('/create', name: 'categorie.create')]
+    public function create(Request $request, CategorieRepository $repository, EntityManagerInterface $em): Response
+    {
+       $categorie = new Categorie();
+       $categorie->setCode('P9LI')->setDescription('Chambre pour 9 persons');
+       $em->persist($categorie);
+       $em->flush(); 
+       return $this->render('categorie/index.html.twig',[
+       'categories' => $categories
     ]);
     }
     #[Route('/categories/{code}-{id}', name: 'categorie.show', requirements: ['id' => '\d+','code' => '[a-zA-Z0-9-]+'])]
--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Repository\RecipeRepository;
+use Symfony\Entity\Recipe;
 
 
 final class RecipeController extends AbstractController
